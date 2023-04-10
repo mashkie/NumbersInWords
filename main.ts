8bbee9c23d62b639f8aedbefbb6360fbb76f056f
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2023-04-10 16:44:09 +0200

    add conversions up to one billion-1

--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
 export function convertNumberToWord(numberInput: number): string {
   numberInput = +numberInput;
-  if (isNaN(numberInput))
-    throw new Error("The number has to be a valid integer");
-  if (numberInput < 0 || numberInput > 999999999)
-    throw new Error("Number has to be positive and be bellow 999.999.999");
-  if (numberInput % 1 !== 0)
-    throw new Error("Number can not contain a decimal point");
 
-  const numberLength = numberInput.toString().length;
+  if (isNaN(numberInput)) {
+    throw new Error("The number has to be a valid integer");
+  }
+  if (numberInput < 0 || numberInput > 999999999) {
+    throw new Error("Number has to be positive and be bellow 999.999.999");
+  }
+  if (numberInput % 1 !== 0) {
+    throw new Error("Number can not contain a decimal point");
+  }
+
   const singleDigits = [
-    "zero",
+    "",
     "one",
     "two",
     "three",
@@ -21,7 +24,6 @@ export function convertNumberToWord(numberInput: number): string {
     "nine",
   ];
   const teens = [
-    "",
     "ten",
     "eleven",
     "twelve",
@@ -33,8 +35,52 @@ export function convertNumberToWord(numberInput: number): string {
     "eighteen",
     "nineteen",
   ];
-  if (numberLength === 1) {
-    return singleDigits[numberInput.toString().charCodeAt(0) - 48];
+  const tens = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "fourty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ];
+
+  let numberAsWord = "";
+
+  if (numberInput === 0) {
+    return "zero";
   }
-  return "zero";
+
+  if (numberInput >= 1000000) {
+    numberAsWord +=
+      convertNumberToWord(Math.floor(numberInput / 1000000)) + " million ";
+    numberInput %= 1000000;
+  }
+
+  if (numberInput >= 1000) {
+    numberAsWord +=
+      convertNumberToWord(Math.floor(numberInput / 1000)) + " thousand ";
+    numberInput %= 1000;
+  }
+
+  if (numberInput >= 100) {
+    numberAsWord += singleDigits[Math.floor(numberInput / 100)] + " hundred ";
+    numberInput %= 100;
+  }
+
+  if (numberInput >= 10 && numberInput <= 19) {
+    numberAsWord += teens[numberInput - 10];
+    return numberAsWord.trim();
+  }
+  if (numberInput >= 20) {
+    numberAsWord += tens[Math.floor(numberInput / 10)] + " ";
+    numberInput %= 10;
+  }
+  if (numberInput > 0) {
+    numberAsWord += singleDigits[numberInput];
+  }
+  return numberAsWord.trim();
 }
--- main_test.ts
+++ main_test.ts
@@ -15,7 +15,7 @@ describe("main", () => {
       expect(convertNumberToWord("0")).toEqual("zero");
     });
 
-    it("should throw an error if a string was passed which is not convertable to a number", () => {
+    it("should throw an error if a string was passed which is not convert able to a number", () => {
       expect(() => {
         convertNumberToWord("1EUR");
       }).toThrow("The number has to be a valid integer");
@@ -40,14 +40,13 @@ describe("main", () => {
     });
   });
 
-  describe("conversion in the single digits", () => {
-    it("should convert a number as a single digit if its length equals 1", () => {
+  describe("conversion till 999", () => {
+    it("should convert the number 0", () => {
       expect(convertNumberToWord(0)).toEqual("zero");
     });
 
-    it("should convert the numbers from 0 to 9", () => {
+    it("should convert the numbers from 1 to 9", () => {
       const results = [
-        "zero",
         "one",
         "two",
         "three",
@@ -58,7 +57,7 @@ describe("main", () => {
         "eight",
         "nine",
       ];
-      const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+      const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
       numbers.forEach((num, index) => {
         expect(convertNumberToWord(num)).toEqual(results[index]);
       });
@@ -76,11 +75,94 @@ describe("main", () => {
         "eighteen",
         "nineteen",
       ];
-      const numbers = [10,11,12,13,14,15,16,17,18,19];
+      const numbers = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
       numbers.forEach((num, index) => {
-        console.log("num", num);
-        console.log("index", results[index]);
-        
+        expect(convertNumberToWord(num)).toEqual(results[index]);
+      });
+    });
+    it("should convert all numbers in the tens up to 90", () => {
+      const results = [
+        "twenty",
+        "thirty",
+        "fourty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+      ];
+      const numbers = [20, 30, 40, 50, 60, 70, 80, 90];
+      numbers.forEach((num, index) => {
+        expect(convertNumberToWord(num)).toEqual(results[index]);
+      });
+    });
+    it("should convert all numbers in the hundreds up to 900", () => {
+      const results = [
+        "one hundred",
+        "two hundred",
+        "three hundred",
+        "four hundred",
+        "five hundred",
+        "six hundred",
+        "seven hundred",
+        "eight hundred",
+        "nine hundred",
+      ];
+      const numbers = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+      numbers.forEach((num, index) => {
+        expect(convertNumberToWord(num)).toEqual(results[index]);
+      });
+    });
+    it("should convert numbers in between", () => {
+      const results = [
+        "twenty one",
+        "thirty two",
+        "fourty three",
+        "fifty four",
+        "one hundred one",
+        "two hundred twenty two",
+        "three hundred thirty three",
+        "nine hundred ninety nine",
+      ];
+      const numbers = [21, 32, 43, 54, 101, 222, 333, 999];
+      numbers.forEach((num, index) => {
+        expect(convertNumberToWord(num)).toEqual(results[index]);
+      });
+    });
+  });
+  describe("conversions in the thousands", () => {
+    it("should call itself recursively until the number in the thousands is converted", () => {
+      const results = [
+        "one thousand",
+        "two thousand one",
+        "three thousand sixteen",
+        "four thousand twenty one",
+        "five thousand one hundred one",
+        "six thousand two hundred twelve",
+        "six thousand two hundred twenty two",
+        "seven thousand three hundred thirty three",
+        "nine thousand nine hundred ninety nine",
+      ];
+      const numbers = [1000, 2001, 3016, 4021, 5101, 6212, 6222, 7333, 9999];
+      numbers.forEach((num, index) => {
+        expect(convertNumberToWord(num)).toEqual(results[index]);
+      });
+    });
+  });
+  describe("conversions in the millions", () => {
+    it("should call itself recursively until the number in the millions is converted", () => {
+      const results = [
+        "one million",
+        "two million one",
+        "three million sixteen",
+        "four million two hundred twenty one",
+        "five million six thousand eight hundred ninety nine",
+        "twenty one million one hundred sixty seven thousand eight hundred ninety one",
+        "one hundred twenty three million four hundred fifty six thousand seven hundred eighty nine",
+        "nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine",
+      ];
+      const numbers = [1000000, 2000001, 3000016, 4000221, 5006899, 21167891, 123456789, 999999999];
+      numbers.forEach((num, index) => {
         expect(convertNumberToWord(num)).toEqual(results[index]);
       });
     });
